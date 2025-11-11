7ac06e8a0880e1c46f180c16f88a58d32cfbd1f9
Wilhelm Murdoch <wilhelm@devilmayco.de>
2025-11-11 13:05:48 +1000

    small renaqme

--- internal/diagnostics/manager.go
+++ internal/diagnostics/manager.go
@@ -6,21 +6,23 @@ import (
 	"github.com/hashicorp/hcl/v2"
 )
 
+// DiagnosticsManager embeds the structure of hcl.Diagnostics and combines it
+// with a DiagnosticsWriter to simplify use.
 type DiagnosticsManager struct {
 	hcl.Diagnostics
-	DiagnosticWriter hcl.DiagnosticWriter
+	Writer hcl.DiagnosticWriter
 }
 
 // Write is responsible for writing the diagnostics to the DiagnosticWriter.
 func (dm *DiagnosticsManager) Write() error {
-	return dm.DiagnosticWriter.WriteDiagnostics(dm.Diagnostics)
+	return dm.Writer.WriteDiagnostics(dm.Diagnostics)
 }
 
 // NewDiagnosticsManager is responsible for creating a new DiagnosticsManager instance.
 func New(filePath string, file *hcl.File) *DiagnosticsManager {
 	return &DiagnosticsManager{
 		Diagnostics: hcl.Diagnostics{},
-		DiagnosticWriter: hcl.NewDiagnosticTextWriter(
+		Writer: hcl.NewDiagnosticTextWriter(
 			os.Stdout,
 			map[string]*hcl.File{filePath: file},
 			78,
