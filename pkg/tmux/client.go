c83bed0db72c6ee9e3375cac65fb592f8b14fb05
Wilhelm Murdoch <wilhelm@devilmayco.de>
2026-07-17 15:48:29 +1000

    oh. my. god.

--- pkg/tmux/client.go
+++ pkg/tmux/client.go
@@ -214,13 +214,6 @@ func (c Client) NewWindowFromLine(line string, session *Session) (*Window, error
 	if err != nil {
 		return nil, err
 	}
-	baseIndexCmdParts, err := session.Client.GetBaseIndex(session.Target(), "base-index")
-	if err != nil {
-		return nil, err
-	}
-	if len(baseIndexCmdParts) != 2 {
-		return nil, errors.New("could not determine window base index")
-	}
 
 	baseIndexCmdParts, err := session.Client.GetBaseIndex(session.Target(), "base-index")
 	if err != nil {
